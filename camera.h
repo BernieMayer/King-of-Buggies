91d3814e7801ca2aeca9bf70859448a74e8f73a0
JeremyAdamHart <jeremyadamhart@gmail.com>
2016-01-14 20:14:51 -0700

    Create camera.h

---
+++ camera.h
@@ -0,0 +1,56 @@
+#include "glv3f.h"
+#include "matrix.h"
+#include <glm/glm.hpp>
+
+#define minZoom 0.05
+#define maxZoom 5.0
+
+const int FREEROAM_CAMERA = 0;
+const int MODELVIEWER_CAMERA = 1;
+
+class Camera
+{
+	vec3 dir;
+	vec3 up;
+	vec3 right;
+	vec3 pos;
+	vec3 viewCenter;
+
+	int cameraMode;
+
+public:
+	Camera();
+
+	Camera(vec3 _dir, vec3 _up, vec3 _pos);
+
+	Camera(vec3 _dir, vec3 _up, vec3 _pos, int _cameraMode);
+	
+	vec3 getPos();
+
+	vec3 getDir();
+
+	void changeDir(vec3 _dir);
+
+	void changePos(vec3 _pos);
+
+	void changeViewCenter(vec3 _viewCenter);
+
+	mat4 getMatrix();
+
+	void moveForward(float z);
+
+	void moveRight(float x);
+
+	void moveUp(float y);
+	
+	void move(vec3 direction);
+
+	void moveCenter(vec3 direction);
+
+	void rotateView(float x, float y);
+
+	void rotateViewAround(float x, float y);
+
+	void zoom(float scale);
+
+};
