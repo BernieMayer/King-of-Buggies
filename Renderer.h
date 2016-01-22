3e31f6c97c2dde5807d35657e708cdaa483e4514
Jeremy Hart <jeremyadamhart@gmail.com>
2016-01-22 00:29:29 -0700

    Renderer
    Added some of the functions in the renderer framework

--- Renderer.h
+++ Renderer.h
@@ -1,9 +1,29 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
+#int
+
+#include <glm/glm.hpp>
+#include <vector>
 #include "MeshObject.h"
 
+using namespace std;
+using namespace glm;
+
+enum SHADOW_BEHAVIOUR {CAST=0, RECEIVE, CAST_AND_RECEIVE};
+
 class Renderer {
+public:
+	unsigned int generateDrawableObject();
+	void assignMesh(unsigned int object, vector<vec3>* mesh);
+	void assignNormals(unsigned int object, vector<vec3>* normals);
+	void assignUVs(unsigned int object, vector<vec2>* uvs);
+	void assignIndices(unsigned int object, vector<unsigned int>* indices);
+	//void assignMaterial(unsigned int object, Material mat);		//Make material class
+
+	void setShadowBehaviour(int object, int behaviour);
+
+
 
 };
 
