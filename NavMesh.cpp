4fe0e669e7ea85d5cb6c8091117048804646d5a0
Jeremy Hart <jeremyadamhart@gmail.com>
2016-02-26 01:04:16 -0700

    NavMesh improvements
    Still no A*, but we have an implicit edge iterpretation

--- NavMesh.cpp
+++ NavMesh.cpp
@@ -3,6 +3,8 @@
 
 #include "NavMesh.h"
 
+string navMeshDirectory = "./models/";
+
 Edges::Edges()
 {
 
@@ -15,6 +17,11 @@ void Edges::setSize(unsigned int size)
 	{
 		edgeMatrix[i].resize(size, 0);
 	}
+}
+
+void Edges::clear() 
+{
+	edgeMatrix.clear();
 }
 
 Node::Node()
@@ -43,11 +50,11 @@ void Node::addPoints(vector<vec3> _points)
 
 vector<vec3> Node::getPoints() { return points; }
 
-unsigned int Node::numVertices() { return points.size(); }
+unsigned int Node::numVertices() const { return points.size(); }
 
 void Node::calculateCenter()
 {
-	vec3 center = vec3(0.f);
+	center = vec3(0.f);
 
 	for (vector<vec3>::iterator it = points.begin(); it != points.end(); it++)
 	{
@@ -90,11 +97,144 @@ NavMesh::NavMesh()
 
 }
 
+void NavMesh::clear()
+{
+	nodes.clear();
+	edges.clear();
+}
 
 
+bool NavMesh::loadNavMesh(string fileName)
+{
+	clear();
 
+	printf("Loading navigation mesh\n");
+	
+	std::ifstream f(fileName);
+	if (!f.is_open())
+	{
+		printf("Configuration file could not be opened\n");
+		return false;
+	}
 
+	vector<vec3> vertices;
+	vector < vector<unsigned int>> indices;
 
+	std::string line;
+	vec3 temp;
+
+	while (getline(f, line))
+	{
+		//Add vertex
+		if (sscanf(line.c_str(), "v %f %f %f", &temp.x, &temp.y, &temp.z) == 3)
+			vertices.push_back(temp);
+		//Add index
+		else if (line.find("f") != string::npos)
+		{
+			cout << "f ";
+
+			vector<unsigned int> face;
+			unsigned int index;
+
+			stringstream stream(line);
+			while (stream >> index)
+			{
+				face.push_back(index-1);
+				cout << index - 1 << " ";
+			}
+
+			if (face.size() >= 3)
+				indices.push_back(face);
+			cout << endl;
+		}
+	}
+
+	f.close();
+
+	vector < vector<unsigned int> >::iterator face;
+	//Iterate through faces
+	for (face = indices.begin(); face != indices.end(); face++)
+	{
+
+		Node newNode;
+		//Add each point on face to node
+		for (unsigned int i = 0; i < (*face).size(); i++)
+		{
+			newNode.addPoint(vertices[(*face)[i]]);
+		}
+
+		nodes.push_back(newNode);
+	}
+
+	edges.setSize(nodes.size());
+
+	return true;
+
+}
+
+bool NavMesh::findSimilarEdge(const Node& a, const Node& b, unsigned int* a_index, unsigned int* b_index)
+{
+	unsigned int ind_a, ind_b;
+
+	for (unsigned int i = 0; i < a.numVertices(); i++)
+	{
+		for (unsigned int j = 0; j < b.numVertices(); j++)
+		{
+			if (a[i] == b[j])
+			{
+				ind_a = i;
+				ind_b = j;
+				goto found;
+			}
+		}
+	}
+	
+	return false;
+
+found:
+	if (a[ind_a%a.numVertices()] == b[ind_b%b.numVertices()])
+	{
+		*a_index = ind_a;
+		*b_index = ind_b;
+	}
+
+	return true;
+}
+
+void NavMesh::resizeEdges(unsigned int newSize)
+{
+	edges.setSize(newSize);
+
+	point_a.resize(newSize);
+	point_b.resize(newSize);
+
+	for (unsigned int i = 0; i < newSize; i++)
+	{
+		point_a[i].resize(newSize, vec3(0.f, 0.f, 0.f));
+		point_b[i].resize(newSize, vec3(0.f, 0.f, 0.f));
+	}
+}
+
+void NavMesh::calculateImplicitEdges()
+{
+	resizeEdges(nodes.size());
+
+	unsigned int i_a, i_b;
+	for (unsigned int i = 0; i < nodes.size(); i++)
+	{
+		for (unsigned int j = i; j < nodes.size(); j++)
+		{
+			if (findSimilarEdge(nodes[i], nodes[j], &i_a, &i_b))
+			{
+				edges[i][j] = 1.f;
+				edges[j][i] = 1.f;
+
+				point_a[j][i] = point_a[i][j] = nodes[i][i_a];
+				point_b[j][i] = point_b[i][j] = nodes[i][(i_a+1)%nodes[i].numVertices()];
+			}
+		}
+	}
+}
 
 
 
--- NavMesh.h
+++ NavMesh.h
@@ -3,34 +3,20 @@
 
 #include <glm/glm.hpp>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 using namespace glm;
 
-//Stores edges in matrix form
-//To retrieve weight of edge from i to j, use edges[i][j]
-//0 means no edge between i and j
-class Edges
-{
-private:
-	vector<vector<unsigned int>> edgeMatrix;
-
-public:
-	Edges();
-
-	void setSize(unsigned int size);
-	unsigned int size() { return edgeMatrix.size(); }
-
-	vector<unsigned int> operator[] (unsigned int index) const { return edgeMatrix[index]; }
-	vector<unsigned int>& operator[] (unsigned int index) { return edgeMatrix[index]; }
-
-};
 
 class Node
 {
 private:
 	vector<vec3> points;
-	vector <vec3> center;
+	vec3 center;
 
 	void calculateCenter();
 
@@ -41,12 +27,36 @@ public:
 	void addPoint(vec3 point);
 	void addPoints(vector<vec3> _points);
 
+	vec3 operator[] (unsigned int index) const { return points[index]; }
+	vec3& operator[] (unsigned int index) { return points[index]; }
+
 	vector<vec3> getPoints();
 
-	unsigned int numVertices();
+	unsigned int numVertices() const;
 
 	vec3 getCenter();
 	bool inPolygon(vec3 pos);
+};
+
+//Stores edges in matrix form
+//To retrieve weight of edge from i to j, use edges[i][j]
+//0 means no edge between i and j
+class Edges
+{
+private:
+	vector<vector<float>> edgeMatrix;
+
+public:
+	Edges();
+
+	void setSize(unsigned int size);
+	unsigned int size() const { return edgeMatrix.size(); }
+
+	vector<float> operator[] (unsigned int index) const { return edgeMatrix[index]; }
+	vector<float>& operator[] (unsigned int index) { return edgeMatrix[index]; }
+
+	void clear();
+
 };
 
 
@@ -56,6 +66,9 @@ private:
 	vector<Node> nodes;
 	Edges edges;
 
+	vector<vector<vec3>> point_a;	//First point on edge
+	vector<vector<vec3>> point_b;	//Second point on edge
+
 public:
 	NavMesh();
 
@@ -63,7 +76,14 @@ public:
 	Node& operator[] (unsigned int index) { return nodes[index]; }
 
 	void add(Node newNode) { nodes.push_back(newNode); }
-	void clear() { nodes.clear(); }
+	void clear();
+
+	bool findSimilarEdge(const Node& a, const Node& b, unsigned int* a_index, unsigned int* b_index);
+	void calculateImplicitEdges();
+
+	void resizeEdges(unsigned int newSize);
+
+	bool loadNavMesh(string fileName);
 
 };
 
