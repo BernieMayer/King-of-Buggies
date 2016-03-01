5191217d950412f0ee7229d92810bdd0a09514a6
Shayne Baumgartner <shayneb34@gmail.com>
2016-03-01 16:33:01 -0700

    OpenAL being used!
    OpenAL is now being used to play the music and the engine sounds. The
    old SDL stuff is still there though.

--- SoundManager.h
+++ SoundManager.h
@@ -38,8 +38,21 @@ public:
 private:
 	float map(float value, float min, float max, float newMin, float newMax);
 	int lowestIdleVolume = 10;
+	void initListener(GameState state);
+	void startMusic(GameState state);
+	void startEngineSounds(GameState state);
+	void updateListener(GameState state);
+	void updateMusic(GameState state);
+	void updateEngineSounds(GameState state);
 	void loadWavToBuf(string fileName, ALuint* source, ALuint* buffer);
 	ALfloat* vec3ToALfloat(vec3 vec);
+	ALfloat* vec3ToALfloat(vec3 vector1, vec3 vector2);
+	ALuint musicSource;
+	ALuint engineSources[5];
+	ALuint listener;
+
+	float idleEngineVolume = 0.1;
+	float idleEnginePitch = 1.0;
 };
 
 #endif // SOUNDMANAGER_H
