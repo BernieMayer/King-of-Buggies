22c0a834ac8d336683c3e06d05ce0f37b9274907
sbaumga <shayneb34@gmail.com>
2016-03-14 23:27:02 -0600

    Input smoother now time based, not frame based

--- InputSmoother.cpp
+++ InputSmoother.cpp
@@ -5,6 +5,8 @@
 
 InputSmoother::InputSmoother()
 {
+	timer = Timer();
+	movementTime = timer.getCurrentTime();
 }
 
 
@@ -15,144 +17,165 @@ InputSmoother::~InputSmoother()
 Input InputSmoother::smooth(Input in, bool inAir) {
 	Input out = in;
 
-	// If forward is increasing too much, increase by max instead
-	if (out.forward > lastForward && out.forward - lastForward > accelStep) {
-		out.forward = lastForward + accelStep;
-	}
-	// If forward is decreasing too much, decrease by max instead
-	else if (out.forward < lastForward && lastForward - out.forward > accelStep) {
-		out.forward = lastForward - accelStep;
-	}
-	// Otherwise, do not change
-	lastForward = out.forward;
+	difference = timer.getTimeSince(movementTime);
+	printf("%f\n", difference);
 
-	// If backward is increasing too much, increase by max instead
-	if (out.backward > lastBackward && out.backward - lastBackward > accelStep) {
-		out.backward = lastBackward + accelStep;
-	}
-	// If backward is decreasing too much, decrease by max instead
-	else if (out.backward < lastBackward && lastBackward - out.backward > accelStep) {
-		out.backward = lastBackward - accelStep;
-	}
-	// Otherwise, do not change
-	lastBackward = out.backward;
+	if (difference >= movementCounterEnd) {
+		movementTime = timer.getCurrentTime();
 
-	// If turnL is increasing too much, increase by max instead
-	if (out.turnL > lastTurnL && out.turnL - lastTurnL > turnStep) {
-		out.turnL = lastTurnL + turnStep;
-	}
-	// If turnL is decreasing too much, decrease by max instead
-	else if (out.turnL < lastTurnL && lastTurnL - out.turnL > turnStep) {
-		out.turnL = lastTurnL - turnStep;
-	}
-	// Otherwise, do not change
-	lastTurnL = out.turnL;
+		// If forward is increasing too much, increase by max instead
+		if (out.forward > lastForward && out.forward - lastForward > accelStep) {
+			out.forward = lastForward + accelStep;
+		}
+		// If forward is decreasing too much, decrease by max instead
+		else if (out.forward < lastForward && lastForward - out.forward > accelStep) {
+			out.forward = lastForward - accelStep;
+		}
+		// Otherwise, do not change
+		lastForward = out.forward;
 
-	// If turnR is increasing too much, increase by max instead
-	if (out.turnR > lastTurnR && out.turnR - lastTurnR > turnStep) {
-		out.turnR = lastTurnR + turnStep;
-	}
-	// If turnR is decreasing too much, decrease by max instead
-	else if (out.turnR < lastTurnR && lastTurnR - out.turnR > turnStep) {
-		out.turnR = lastTurnR - turnStep;
-	}
-	// Otherwise, do not change
-	lastTurnR = out.turnR;
+		// If backward is increasing too much, increase by max instead
+		if (out.backward > lastBackward && out.backward - lastBackward > accelStep) {
+			out.backward = lastBackward + accelStep;
+		}
+		// If backward is decreasing too much, decrease by max instead
+		else if (out.backward < lastBackward && lastBackward - out.backward > accelStep) {
+			out.backward = lastBackward - accelStep;
+		}
+		// Otherwise, do not change
+		lastBackward = out.backward;
 
-	if (inAir) {
-		if (out.tiltForward > lastTiltForward && out.tiltForward - lastTiltForward > inAirControlStep) {
-			out.tiltForward = lastTiltForward + inAirControlStep;
+		// If turnL is increasing too much, increase by max instead
+		if (out.turnL > lastTurnL && out.turnL - lastTurnL > turnStep) {
+			out.turnL = lastTurnL + turnStep;
 		}
-		else if (out.tiltForward < lastTiltForward && lastTiltForward - out.tiltForward > inAirControlStep) {
-			out.tiltForward = lastTiltForward - inAirControlStep;
+		// If turnL is decreasing too much, decrease by max instead
+		else if (out.turnL < lastTurnL && lastTurnL - out.turnL > turnStep) {
+			out.turnL = lastTurnL - turnStep;
 		}
-		lastTiltForward = out.tiltForward;
+		// Otherwise, do not change
+		lastTurnL = out.turnL;
 
-		if (out.tiltBackward > lastTiltBackward && out.tiltBackward - lastTiltBackward > inAirControlStep) {
-			out.tiltBackward = lastTiltBackward + inAirControlStep;
+		// If turnR is increasing too much, increase by max instead
+		if (out.turnR > lastTurnR && out.turnR - lastTurnR > turnStep) {
+			out.turnR = lastTurnR + turnStep;
 		}
-		else if (out.tiltBackward < lastTiltBackward && lastTiltBackward - out.tiltBackward > inAirControlStep) {
-			out.tiltBackward = lastTiltBackward - inAirControlStep;
+		// If turnR is decreasing too much, decrease by max instead
+		else if (out.turnR < lastTurnR && lastTurnR - out.turnR > turnStep) {
+			out.turnR = lastTurnR - turnStep;
 		}
-		lastTiltBackward = out.tiltBackward;
+		// Otherwise, do not change
+		lastTurnR = out.turnR;
 
-		if (out.rollL > lastRollL && out.rollL - lastRollL > inAirControlStep) {
-			out.rollL = lastRollL + inAirControlStep;
-		}
-		else if (out.rollL < lastRollL && lastRollL - out.rollL > inAirControlStep) {
-			out.rollL = lastRollL - inAirControlStep;
-		}
-		lastRollL = out.rollL;
+		if (inAir) {
+			if (out.tiltForward > lastTiltForward && out.tiltForward - lastTiltForward > inAirControlStep) {
+				out.tiltForward = lastTiltForward + inAirControlStep;
+			}
+			else if (out.tiltForward < lastTiltForward && lastTiltForward - out.tiltForward > inAirControlStep) {
+				out.tiltForward = lastTiltForward - inAirControlStep;
+			}
+			lastTiltForward = out.tiltForward;
 
-		if (out.rollR > lastRollR && out.rollR - lastRollR > inAirControlStep) {
-			out.rollR = lastRollR + inAirControlStep;
-		}
-		else if (out.rollR < lastRollR && lastRollR - out.rollR > inAirControlStep) {
-			out.rollR = lastRollR - inAirControlStep;
-		}
-		lastRollR = out.rollR;
+			if (out.tiltBackward > lastTiltBackward && out.tiltBackward - lastTiltBackward > inAirControlStep) {
+				out.tiltBackward = lastTiltBackward + inAirControlStep;
+			}
+			else if (out.tiltBackward < lastTiltBackward && lastTiltBackward - out.tiltBackward > inAirControlStep) {
+				out.tiltBackward = lastTiltBackward - inAirControlStep;
+			}
+			lastTiltBackward = out.tiltBackward;
 
-		if (out.spinL > lastSpinL && out.spinL - lastSpinL > inAirControlStep) {
-			out.spinL = lastSpinL + inAirControlStep;
-		}
-		else if (out.spinL < lastSpinL && lastSpinL - out.spinL > inAirControlStep) {
-			out.spinL = lastSpinL - inAirControlStep;
-		}
-		lastSpinL = out.spinL;
+			if (out.rollL > lastRollL && out.rollL - lastRollL > inAirControlStep) {
+				out.rollL = lastRollL + inAirControlStep;
+			}
+			else if (out.rollL < lastRollL && lastRollL - out.rollL > inAirControlStep) {
+				out.rollL = lastRollL - inAirControlStep;
+			}
+			lastRollL = out.rollL;
 
-		if (out.spinR > lastSpinR && out.spinR - lastSpinR > inAirControlStep) {
-			out.spinR = lastSpinR + inAirControlStep;
+			if (out.rollR > lastRollR && out.rollR - lastRollR > inAirControlStep) {
+				out.rollR = lastRollR + inAirControlStep;
+			}
+			else if (out.rollR < lastRollR && lastRollR - out.rollR > inAirControlStep) {
+				out.rollR = lastRollR - inAirControlStep;
+			}
+			lastRollR = out.rollR;
+
+			if (out.spinL > lastSpinL && out.spinL - lastSpinL > inAirControlStep) {
+				out.spinL = lastSpinL + inAirControlStep;
+			}
+			else if (out.spinL < lastSpinL && lastSpinL - out.spinL > inAirControlStep) {
+				out.spinL = lastSpinL - inAirControlStep;
+			}
+			lastSpinL = out.spinL;
+
+			if (out.spinR > lastSpinR && out.spinR - lastSpinR > inAirControlStep) {
+				out.spinR = lastSpinR + inAirControlStep;
+			}
+			else if (out.spinR < lastSpinR && lastSpinR - out.spinR > inAirControlStep) {
+				out.spinR = lastSpinR - inAirControlStep;
+			}
+			lastSpinR = out.spinR;
 		}
-		else if (out.spinR < lastSpinR && lastSpinR - out.spinR > inAirControlStep) {
-			out.spinR = lastSpinR - inAirControlStep;
+		else {
+			lastTiltForward = 0;
+			lastTiltBackward = 0;
+			lastRollL = 0;
+			lastRollR = 0;
+			lastSpinL = 0;
+			lastSpinR = 0;
 		}
-		lastSpinR = out.spinR;
 	}
 	else {
-		lastTiltForward = 0;
-		lastTiltBackward = 0;
-		lastRollL = 0;
-		lastRollR = 0;
-		lastSpinL = 0;
-		lastSpinR = 0;
+		out.forward = lastForward;
+		out.backward = lastBackward;
+		out.turnL = lastTurnL;
+		out.turnR = lastTurnR;
+		out.tiltForward = lastTiltForward;
+		out.tiltBackward = lastTiltBackward;
+		out.rollL = lastRollL;
+		out.rollR = lastRollR;
+		out.spinL = lastSpinL;
+		out.spinR = lastSpinR;
 	}
+ 
+	
 
-	// Menu can only be "pressed" once every 20 frames to prevent
+	// Menu can only be "pressed" once every 5/60ths of a second to prevent
 	// effects triggering on button pressed happening too often
 	if (out.menu && !menuLock) {
 		menuLock = true;
+		menuTime = timer.getCurrentTime();
 	}
-	else if (menuLock) {
+	else if (menuLock && timer.getTimeSince(menuTime) < menuCounterMax) {
 		out.menu = false;
-		menuFrameCounter += 1;
 	}
-	if (menuFrameCounter == menuCounterMax) {
-		menuFrameCounter = 0;
+	else if (menuLock && timer.getTimeSince(menuTime) >= menuCounterMax) {
+		out.menu = false;
 		menuLock = false;
 	}
 
 	if (out.powerup && !powerupLock) {
 		powerupLock = true;
+		powerupTime = timer.getCurrentTime();
 	}
-	else if (powerupLock) {
+	else if (powerupLock && timer.getTimeSince(powerupTime) < powerupCounterMax) {
 		out.powerup = false;
-		powerupFrameCounter += 1;
 	}
-	if (powerupFrameCounter == powerupCounterMax) {
-		powerupFrameCounter = 0;
+	else if (powerupLock && timer.getTimeSince(powerupTime) >= powerupCounterMax) {
+		out.powerup = false;
 		powerupLock = false;
 	}
 
 	
 	if (out.jump && !jumpLock) {
 		jumpLock = true;
+		jumpTime = timer.getCurrentTime();
 	}
-	else if (jumpLock) {
+	else if (jumpLock && timer.getTimeSince(jumpTime) < jumpCounterMax) {
 		out.jump = false;
-		jumpFrameCounter += 1;
 	}
-	if (jumpFrameCounter == jumpCounterMax) {
-		jumpFrameCounter = 0;
+	else if (jumpLock && timer.getTimeSince(jumpTime) >= jumpCounterMax) {
+		out.jump = false;
 		jumpLock = false;
 	}
 	
--- InputSmoother.h
+++ InputSmoother.h
@@ -2,6 +2,10 @@
 #define INPUTSMOOTHER_H
 
 #include "Input.h"
+#include "Timer.h"
+#include <iostream>
+
+using namespace std;
 
 class InputSmoother
 {
@@ -10,34 +14,42 @@ public:
 	~InputSmoother();
 	Input smooth(Input in, bool inAir);
 
+	float difference;
+
 private:
-	float lastForward = 0;
-	float lastBackward = 0;
-	float lastTurnL = 0;
-	float lastTurnR = 0;
-	float lastSpinL = 0;
-	float lastSpinR = 0;
-	float lastRollL = 0;
-	float lastRollR = 0;
-	float lastTiltForward = 0;
-	float lastTiltBackward = 0;
+	float lastForward = 0.f;
+	float lastBackward = 0.f;
+	float lastTurnL = 0.f;
+	float lastTurnR = 0.f;
+	float lastSpinL = 0.f;
+	float lastSpinR = 0.f;
+	float lastRollL = 0.f;
+	float lastRollR = 0.f;
+	float lastTiltForward = 0.f;
+	float lastTiltBackward = 0.f;
 	// Set higher to have faster changes
 	// Lower to have smoother changes
 	float accelStep = 0.08f;
 	float turnStep = 0.08f;
 	float inAirControlStep = 0.3f;
 
+	Timer timer;
+	timeb movementTime;
+	// 1/60th of a second since this seemed to work niceley at 60 frames a second
+	float movementCounterEnd = 1.0f / 60.0f;
+
 	bool menuLock = false;
-	int menuFrameCounter = 0;
-	int menuCounterMax = 5;
+	timeb menuTime;
+	// 5/60th of a second since 5 frames seemed to work nicely
+	float menuCounterMax = 5.0f / 60.0f;
 
 	bool powerupLock = false;
-	int powerupFrameCounter = 0;
-	int powerupCounterMax = 5;
+	timeb powerupTime;
+	float powerupCounterMax = 5.0f / 60.0f;
 
 	bool jumpLock = false;
-	int jumpFrameCounter = 0;
-	int jumpCounterMax = 5;
+	timeb jumpTime;
+	float jumpCounterMax = 5.0f / 60.0f;
 };
 
 #endif // INPUTSMOOTHER_H
